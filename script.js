15f52b6336a0859dd51c8d1cdedeb73c37490889
Jude Nadar <u1588262@utah.edu>
2026-04-18 00:46:59 +0000

    save chatbot updates before merge

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const userInput = document.getElementById("userInput");
 const chatWindow = document.getElementById("chatWindow");
 
 // Set initial message
-chatWindow.textContent = "👋 Hello! How can I help you today?";
+chatWindow.textContent = "Yo How can I help you today?";
 
 /* Handle form submit */
 chatForm.addEventListener("submit", (e) => {
